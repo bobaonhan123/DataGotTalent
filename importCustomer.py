50d394cbc5ebed0e458ee67a4b6d4d4d74b29e8d
Bit0 <hosybaonhan2004@gmail.com>
2024-01-24 17:19:58 +0700

    update customer data processing

--- importCustomer.py
+++ importCustomer.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pymongo import MongoClient
 import os
+from unidecode import unidecode
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['filmlover']
@@ -12,5 +13,10 @@ data = pd.read_excel(file_path)
 data_dict = data.to_dict("records")
 
 collection_name = os.path.splitext(os.path.basename(file_path))[0]
+
+
+
+for data in data_dict:
+    data['address'] = unidecode(str(data['address'])).upper()
 
 db[collection_name].insert_many(data_dict)
